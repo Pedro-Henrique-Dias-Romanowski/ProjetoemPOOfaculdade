d00693d3f4a528d5f196bd10d2b24db50b50b16a
Pedro Henrique Dias Romanowksi <pedro.romanowski02@gmail.com>
2024-07-03 00:40:21 -0300

    Projeto atualizado

--- src/modelo/Casa.java
+++ src/modelo/Casa.java
@@ -1,8 +1,11 @@
 package modelo;
 
+import java.util.Scanner;
+
 public class Casa extends Financiamento{
     public  double tamanhoArea;
     private double tamanhoTerreno;
+    private double acrescimo;
 
     public Casa(double valorestimadoImovel, int prazoFinanciamentoanual, double taxadeJurosAnual, double tamanhoArea, double tamanhoTerreno){
        this.valorImovel = valorestimadoImovel;
@@ -12,9 +15,31 @@ public class Casa extends Financiamento{
        this.tamanhoTerreno = tamanhoTerreno;
     }
 
+    public  double valorMaiorQueMensalidade(double valorJuros, double valorAcrescimo) throws DescontoMaiorDoQueJurosException{
+
+        if(valorAcrescimo > valorJuros){
+               acrescimo = valorAcrescimo = valorJuros;
+            throw new DescontoMaiorDoQueJurosException("O valor do acréscimo não pode ser maior do que o valor do juros");
+        }
+         taxaJurosAnual = valorJuros;
+         return this.acrescimo = valorAcrescimo;
+
+    }
+
     @Override
     public  double calular_pagamento_mensal() {
-        return  (this.valorImovel / (this.prazoFinanciamento * 12)) * (1 + (this.taxaJurosAnual / 12)) + 80;
+        // obter o valor dos juros
+         taxaJurosAnual = calular_pagamento_mensal() * (getTaxaJurosAnual() / 12);
+        // obter o valor dos acréscimos
+        double valorDoAcrescimo = acrescimo;
+        try{
+            valorMaiorQueMensalidade(100, 80);
+        }  catch (DescontoMaiorDoQueJurosException e){
+            double valorParaIgualar = valorDoAcrescimo = taxaJurosAnual;
+            System.out.println(e.getMessage());
+        }
+
+        return  (this.valorImovel / (this.prazoFinanciamento * 12)) * (1 + (this.taxaJurosAnual / 12)) + this.acrescimo;
 
     }
 
@@ -25,7 +50,7 @@ public class Casa extends Financiamento{
 
     @Override
     public void mostrarDadosFinanciamento() {
-        System.out.printf("O valor total vai ser de: R$%.2f" , this.calcular_total_pagamento(), ",");
+        System.out.printf("O valor total vai ser de: R$%.2f" , this.calcular_total_pagamento());
         System.out.printf("O valor do imóvel  vai ser de: R$%.2f" , this.getValorImovel());
     }
 
--- src/util/InterfaceUsuario.java
+++ src/util/InterfaceUsuario.java
@@ -8,33 +8,39 @@ public class InterfaceUsuario {
 
     // Esse método vai solicitar para o usuário digitar o valor do imóvel
     public double pedirValorImovel() {
-        double valorImovel;
+        double valorImovel = 0;
         do{
-            System.out.println("Qual é o valor do ímovel?");
-            valorImovel = scanner.nextDouble();
-            if(valorImovel <= 0 ){
-                System.out.println("O valor do imóvel não pode ser negativo e nem igual a zero, tente novamnete");
-            } else if(valorImovel > 100000000){
-                System.out.println("Valor do imóvel muito alto tente novamente");
-            }
-        } while(valorImovel <= 0 || valorImovel > 100000000);
-        return valorImovel;
+            try{
+                System.out.println("Qual será o valor do imóvel? ");
+                valorImovel = scanner.nextDouble();
+                if(valorImovel <= 0 || valorImovel > 1000000){
+                    System.out.println("Valor inválido, tente novamente");
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
 
+            }
+        } while( valorImovel > 1000000);
+        return valorImovel;
     }
+
 
 
     // Esse método vai solicitar ao usário para ele digitar o valor do financiamento
     public int pedirPrazoFinanciamento() {
-        int prazoFinanciamento ;
+        int prazoFinanciamento = 0;
         do{
-            System.out.println("Em quantos anos irá ser o financiamento?");
-            prazoFinanciamento = scanner.nextInt();
-            if (prazoFinanciamento <= 0) {
-                System.out.println("O valor do financiamneto não pode ser negativo ou igual a zero, tente novamante");
-            } else if(prazoFinanciamento > 20){
-                System.out.println("O prazo do financiamento não pode ser maior  do que 20 anos, tente novamente");
+            try{
+                System.out.println("Em quantos anos será o financiamento?");
+                prazoFinanciamento = scanner.nextInt();
+                if(prazoFinanciamento <= 0 || prazoFinanciamento > 20){
+                    System.out.println("Prazo inválido, tente novamente");
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
             }
-        } while (prazoFinanciamento<= 0 || prazoFinanciamento > 20);
+        } while(prazoFinanciamento <= 0 || prazoFinanciamento > 20);
+
         return prazoFinanciamento;
     }
 
@@ -42,85 +48,121 @@ public class InterfaceUsuario {
 
     // Esse método vai solicitar ao usuário para que ele digite o valor da taxa de juros
     public double pedirTaxadejuros() {
-        double taxaJurosAnual ;
-        do {
-            System.out.println("Qual é o valor da porcentagem da taxa de juros?");
-            taxaJurosAnual = scanner.nextDouble();
-            if (taxaJurosAnual<= 0) {
-                System.out.println("O valor da taxa de juros não pode ser negativo e nem igual a zero, tente novamente");
-            } else if(taxaJurosAnual > 20){
-                System.out.println("Taxa de juros muito alta, tente novamente");
+
+        double taxaJurosAnual = 0;
+        do{
+            try{
+                System.out.println("Qual é o valor da porcentagem da taxa de juros? ");
+                taxaJurosAnual = scanner.nextDouble();
+                if(taxaJurosAnual <= 0 || taxaJurosAnual > 20){
+                    System.out.println("Taxa de juros imválida, tente novamente");
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
             }
         } while(taxaJurosAnual <= 0 || taxaJurosAnual > 20);
         return taxaJurosAnual;
 
     }
 
+
+    // Esse método vai pedir ao usuário o tamanho da área do terreno
     public double pedirTamanhoArea(){
-         double tamanhoArea;
-         do{
-             System.out.println("Qual é o tamanho da área construída(Em metros qudrados)");
-             tamanhoArea = scanner.nextDouble();
-             if(tamanhoArea > 500000){
-                 System.out.println("Tamanho da área muito grande, tente novamente");
-             } else if(tamanhoArea < 100){
-                 System.out.println("O tamanho da área deve ser maior, tente novamente");
-             }
-         } while(tamanhoArea > 500000 || tamanhoArea < 100 );
-           return  tamanhoArea;
+        double tamamhoArea = 0;
+        do{
+            try{
+                System.out.println("Qual será o tamanho da área construída?(Em metros quadrados)");
+                tamamhoArea = scanner.nextDouble();
+                if(tamamhoArea > 500000 || tamamhoArea < 100){
+                    System.out.println("Tamanho da área invalida, tente novamente");
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        } while(tamamhoArea > 500000 || tamamhoArea < 100);
+        return tamamhoArea;
     }
+
+
+
+    // Esse método vai pedir ao usuário o tamanho do terreno
     public double pedirTamanhoTerreno(){
-         double tamanhoTerreno;
-         do{
-             System.out.println("Qual será o tamanho do terreno?(Em metros quadrados)");
-             tamanhoTerreno = scanner.nextDouble();
-             if(tamanhoTerreno > 1000000){
-                 System.out.println("Tamanho do terreno muito grande, tente novamente");
-             } else if(tamanhoTerreno < 100){
-                 System.out.println("O tamanho do terreno deve ser maior, tente novamente");
-             }
-         } while(tamanhoTerreno > 1000000 || tamanhoTerreno < 100);
-         return tamanhoTerreno;
+        double tamanhoTerreno = 0;
+        do{
+            try{
+                System.out.println("Qual será o tamanho do terreno?(Em metros quadrados)");
+                tamanhoTerreno = scanner.nextDouble();
+                if(tamanhoTerreno > 1000000 || tamanhoTerreno < 100){
+                    System.out.println("Tamanho do terreno inválido, tente novamente");
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        } while (tamanhoTerreno > 1000000 || tamanhoTerreno < 100);
+        return tamanhoTerreno;
+
     }
 
+
+
+
+    // Esse método vai pedir ao usuário a quantidade de vagas na caragem do apartamento
     public int pedirNumeroDeVagasGaragem(){
-         int numeroDeVagas;
-         do{
-             System.out.println("Serão quantas vagas na garagem?");
-             numeroDeVagas = scanner.nextInt();
-             if(numeroDeVagas > 5){
-                 System.out.println("O número de vagas deve ser menor do que 5, tente novamente");
-             } else if (numeroDeVagas < 0){
-                 System.out.println("O número de vagas não pode ser negativo e nem maior doque cinco, tente novamente");
-             }
-         } while(numeroDeVagas > 5 || numeroDeVagas < 0);
-         return numeroDeVagas;
+        int numeroDeVagas = 0;
+        do{
+            try {
+                System.out.println("Serão quantas vagas na caragem?");
+                numeroDeVagas = scanner.nextInt();
+                if(numeroDeVagas > 5 || numeroDeVagas < 0){
+                    System.out.println("Número de vagas inválido, tente novamente");
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        } while(numeroDeVagas > 5 || numeroDeVagas < 0);
+        return numeroDeVagas;
     }
 
+
+
+    // Esse método vai pedir ao usuário o número do andar do apartamento
     public int  pedirAndar(){
-         int andar;
-         do{
-             System.out.println("Qual é o andar do apartamento");
-             andar = scanner.nextInt();
-             if(andar > 25){
-                 System.out.println("O andar deve ser menor do que 25, tente novamente");
-             } else if(andar < 0){
-                 System.out.println("O andar não pode ser negativo, tente novamente");
-             }
-         } while(andar > 25 || andar < 0);
-         return  andar;
+        int andar = 0;
+        do{
+            try{
+                System.out.println("Qual é o andar do apartamento?");
+                andar = scanner.nextInt();
+                if(andar > 25 || andar < 0 ){
+                    System.out.println("Número do andar inválido, tente novamente");
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        } while(andar > 25 || andar < 0);
+        return andar;
     }
 
+
+
+    // Esse método vai pedir ao usuário o tipo de zona do terreno
     public int pedirTipoDeZona(){
-         int  tipodeZona;
-         do{
-             System.out.println("Qual será o tipo do terreno? (1[Residencial]/2[Comercial])");
-             tipodeZona = scanner.nextInt();
-             if(tipodeZona < 1 || tipodeZona > 2){
-                 System.out.println("O tipo de zona deve ser Residencial[1] ou comercial[2], tente novamente");
-             }
-         } while (tipodeZona < 1 || tipodeZona > 2);
-         return tipodeZona;
+        int tipoZona = 0;
+        do{
+            try{
+                System.out.println("Qual será o tipo do terreno? Residencial[1] ou Comercial[2]");
+                tipoZona = scanner.nextInt();
+                if(tipoZona < 1 || tipoZona > 2){
+                    System.out.println("Tipo de terreno inválido, tente novamente");
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        } while(tipoZona < 1 || tipoZona > 2);
+        return tipoZona;
+    }
     }
 
-}
+
+
+
+
